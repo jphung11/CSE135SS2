c1aa1c5e9255cef89c339cc2f3c3e25977df5bdb
Jennifer Phung <jenniferphung11@gmail.com>
2016-08-15 00:25:48 -0700

    collect form files for php

---
+++ echo_php_get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<HTML>
+
+<HEAD>
+<STYLE>
+body {background-color: <?php echo $_GET["color"]; ?>}
+</STYLE>
+<TITLE>Form Results</TITLE>
+</HEAD>
+
+<BODY>
+<H2>Hello <?php echo $_GET["first"]; ?> <?php echo $_GET["last"]; ?> from a Web app written with PHP on data time</H2>
+</BODY>
+
+</HTML>
+
