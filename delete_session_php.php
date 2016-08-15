447013b17dbea5a015f52b5ac826770af17de136
Bryan T <Bryan T>
2016-08-15 08:30:58 -0700

    Updated delete

--- delete_session_php.php
+++ delete_session_php.php
@@ -1,8 +1,9 @@
 <?php
 	session_start();
+	$cookie_name = "DA_BEST_COOKIE_EVA";
 	$users_name = $_SESSION['username']; 
-	unset($_COOKIE[$users_name]);
-	setcookie($users_name,' ', time()-3600);
+	unset($_COOKIE[]);
+	setcookie($cookie_name,$users_name, time()-3600);
 	session_unset();
 	session_destroy();
 
