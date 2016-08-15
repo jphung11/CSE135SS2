36af7c3e86f6aea67ff4b068921c2478cb423ebf
Bryan T <Bryan T>
2016-08-14 18:05:35 -0700

    Hoping sessions for php works

--- session_php.php
+++ session_php.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	    <title>Session Page 1</title>
+	    <title>Session Page 2</title>
 	    <link rel="stylesheet" href="http://138.68.48.65/style.css" type="text/css">
 
 	     <link rel="icon" type="image/png" href="http://138.68.48.65/favicon.png"> 
@@ -12,6 +12,24 @@
 	    <link rel="icon" type="image/png" href="http://138.68.48.65/icons/favicon-16x16.png" sizes="16x16" /> 
 </head>
 <body>
-	<h1>Hello <?php echo $_POST["name"] ?>, nice to meet you!</h1> 
+	<?php
+		if (empty($_SESSION("name"))) {
+			if(empty($_POST["name"])) { 
+				echo << EOT; 
+				<h1>"Howdy stranger...tell me your name on page1!"</h1>
+				EOT
+			}
+			else {
+				session_start();
+				$_SESSION["name"] = $_POST["name"];
+				setcookie($_SESSION["name"], "poop",time()+86400);
+			}
+		}
+		echo << EOT;
+		<h1>Hi $_SESSION["name"] nice to meet you</h1>
+		EOT	
+	?>
+	<button type="button" href="sessionpage1_PHP.php">Back to page one</button>
+	<button type="button" onclick=<?php unset($_COOKIE[$_SESSION["name"]); setcookie($_SESSION["name"],"poop",time()-3600); session_unset(); session_destroy(); ?>>Clear Session</button>
 </body>
 </html>
