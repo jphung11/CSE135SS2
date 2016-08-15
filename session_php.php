a8f94479374ca04fc3fb11143ce1171c0d8e09a5
Bryan T <Bryan T>
2016-08-14 19:10:54 -0700

    p

--- session_php.php
+++ session_php.php
@@ -12,24 +12,24 @@
 	    <link rel="icon" type="image/png" href="http://138.68.48.65/icons/favicon-16x16.png" sizes="16x16" /> 
 </head>
 <body>
-	<?php
-		if (empty($_SESSION("name"))) {
-			if(empty($_POST["name"])) { 
-				echo << EOT; 
-				<h1>"Howdy stranger...tell me your name on page1!"</h1>
-				EOT
-			}
-			else {
-				session_start();
-				$_SESSION["name"] = $_POST["name"];
-				setcookie($_SESSION["name"], "poop",time()+86400);
-			}
-		}
-		echo << EOT;
-		<h1>Hi $_SESSION["name"] nice to meet you</h1>
-		EOT	
-	?>
-	<button type="button" href="sessionpage1_PHP.php">Back to page one</button>
-	<button type="button" onclick=<?php unset($_COOKIE[$_SESSION["name"]); setcookie($_SESSION["name"],"poop",time()-3600); session_unset(); session_destroy(); ?>>Clear Session</button>
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+	if (empty($_POST["name"])) { 
+		echo <<< EOT
+			<h1>Howdy stranger...tell me your name on page1!</h1>
+EOT;
+	}
+	else {
+		session_start();
+		$_SESSION["name"] = $_POST["name"];
+		$name = $_SESSION["name"];
+		setcookie($name, "poop",time()+86400);
+		echo <<< EOT
+			<h1>Hi $name nice to meet you</h1>
+EOT;	
+	}
+} 
+?>
+<a href="sessionpage1_PHP.php">Back to page one</button>
 </body>
 </html>
